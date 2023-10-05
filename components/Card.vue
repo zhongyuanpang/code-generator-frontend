640486c48325d6a0114c72c670a1e7974f27fc14
pzy <pangzy1013@gmail.com>
2023-10-05 18:36:59 +0800

    ~

---
+++ components/Card.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="header">
+            <div class="title">
+
+            </div>
+            <div></div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.card{
+    box-shadow: $default-box-shadow;
+    border: 1px dashed gray;
+
+    .header{
+        line-height: 100px;
+
+    }
+}
+</style>
