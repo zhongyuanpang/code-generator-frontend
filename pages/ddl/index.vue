52ab7eeea4443a8ed31c043064f9f149959b4d46
pzy <pangzy1013@gmail.com>
2023-10-31 20:40:11 +0800

    ~

--- pages/ddl/index.vue
+++ pages/ddl/index.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import {reactive, ref} from 'vue'
 import TableSelect from "~/components/TableSelect.vue";
+import MarkdownIt from 'markdown-it';
+import Prism from 'prismjs'
 import {getTableColumnInfo} from "~/composables/api/template";
+
+//region  《 变量 、 常量 》
 
 // 选择生成数据库类型
 const dataBaseOptions = reactive<any>([
@@ -27,8 +31,17 @@ const result = ref("")
 
 // tabs默认选择
 const value = ref('add');
+//endregion
 
-const currentItem = ref([]);
+
+//region  《 组件 ref 》
+
+const tableSelect = ref();
+//endregion
+
+
+//region  《 事件 》
+
 const handlePanelChange = (val: any) => {
   console.log(val);
 };
@@ -40,8 +53,40 @@ const handlerChange = (newValue: string) => {
 
 // 生成事件
 const generate = (() => {
-
+  switch (value.value){
+    case "add":
+      generateAdd();
+      break
+  }
 })
+
+const generateAdd = () => {
+  const tableName = addFieldFormData.tableName;
+  const fieldList = addFieldFormData.fieldList;
+
+  let str = ``
+//  ALTER TABLE proddmv ADD jzrqcreationdate datetime COMMENT '制作日期';
+  fieldList.forEach(item=>{
+    console.log(item)
+    const {COLUMN_NAME,COLUMN_COMMENT,DATA_TYPE,CHARACTER_MAXIMUM_LENGTH,COLUMN_DEFAULT,IS_NULLABLE,COLUMN_KEY} = item
+    str += `
+ALTER TABLE ${tableName} ADD ${COLUMN_NAME} ${DATA_TYPE} COMMENT '${COLUMN_COMMENT}';
+`
+  })
+
+
+  const mdResult = `
+\`\`\`sql
+${str.slice(0,-1)}
+\`\`\`
+`
+  const md = new MarkdownIt();
+
+  result.value = md.render(mdResult);
+  nextTick(()=>{
+    Prism.highlightAll()
+  })
+}
 
 // 添加字段
 const pushField = (() => {
@@ -50,14 +95,11 @@ const pushField = (() => {
     COLUMN_COMMENT:"",
     DATA_TYPE:"",
     CHARACTER_MAXIMUM_LENGTH:"",
-    IS_NULLABLE:"",
+    IS_NULLABLE:false,
     COLUMN_DEFAULT:"",
     COLUMN_KEY:"",
   })
 })
-
-
-const tableSelect = ref();
 
 // 获取所有表
 const getTable = (()=>{
@@ -87,6 +129,16 @@ const getColumnInfo = (value:string) => {
   //   }
   // })
 }
+
+/**
+ * 移除字段
+ * @param index
+ */
+const removeField = (index: number) => {
+  addFieldFormData.fieldList.splice(index,1)
+}
+
+//endregion
 </script>
 
 <template>
@@ -115,25 +167,33 @@ const getColumnInfo = (value:string) => {
                         <t-input v-model="addFieldFormData.tableName" disabled></t-input>
                       </t-form-item>
                       <div style="max-height: 35vh;overflow: scroll;">
-                      <t-collapse @change="handlePanelChange" v-if="addFieldFormData.fieldList.length">
+                      <t-collapse @change="handlePanelChange" v-if="addFieldFormData.fieldList.length" :expand-on-row-click="false">
                         <t-collapse-panel v-for="(item,index) in addFieldFormData.fieldList" :value="index" :key="index">
                           <template #header>
                             <t-space size="small">
                               <t-input v-model="addFieldFormData.fieldList[index].COLUMN_NAME" placeholder="字段名"></t-input>
                             </t-space>
                           </template>
+                          <template #headerRightContent>
+                              <t-button shape="circle" theme="danger" @click="removeField(index)">
+                                <template #icon><Delete1Icon /></template>
+                              </t-button>
+                          </template>
                           <t-space>
                             <t-form-item label="字段类型" name="DATA_TYPE">
-                              <t-input v-model="addFieldFormData.fieldList[index].DATA_TYPE"></t-input>
+                              <t-input v-model="addFieldFormData.fieldList[index].DATA_TYPE"/>
                             </t-form-item>
                             <t-form-item label="默认值" name="COLUMN_DEFAULT">
-                              <t-input v-model="addFieldFormData.fieldList[index].COLUMN_DEFAULT"></t-input>
+                              <t-input v-model="addFieldFormData.fieldList[index].COLUMN_DEFAULT"/>
                             </t-form-item>
                           </t-space>
                           <div style="height: 10px;"></div>
                           <t-space>
                             <t-form-item label="注释" name="COLUMN_COMMENT">
-                              <t-input v-model="addFieldFormData.fieldList[index].COLUMN_COMMENT"></t-input>
+                              <t-input v-model="addFieldFormData.fieldList[index].COLUMN_COMMENT"/>
+                            </t-form-item>
+                            <t-form-item label="非空" name="IS_NULLABLE">
+                              <t-checkbox v-model="addFieldFormData.fieldList[index].IS_NULLABLE"/>
                             </t-form-item>
                           </t-space>
                         </t-collapse-panel>
@@ -141,15 +201,6 @@ const getColumnInfo = (value:string) => {
                       </div>
                     </t-form>
 
-                    <!--endregion -->
-                    <!--region  《 手风琴 》  -->
-<!--                    <t-space direction="vertical">-->
-<!--                      <t-collapse v-model="currentItem" expand-mutex @change="handlePanelChange">-->
-<!--                        <t-collapse-panel value="0" header="这是一个折叠标题">-->
-<!--                          这部分是每个折叠面板折叠或展开的内容，可根据不同业务或用户的使用诉求，进行自定义填充。可以是纯文本、图文、子列表等内容形式。-->
-<!--                        </t-collapse-panel>-->
-<!--                      </t-collapse>-->
-<!--                    </t-space>-->
                     <!--endregion -->
                     <t-divider />
                     <t-button block size="medium" variant="dashed" @click="pushField">
@@ -224,6 +275,24 @@ const getColumnInfo = (value:string) => {
     <TableSelect ref="tableSelect" @getColumnInfo="getColumnInfo"/>
   </div>
 </template>
+
+<style>
+/* 全局样式，适用于所有滚动条 */
+body::-webkit-scrollbar {
+  width: 0.5em;
+  background-color: transparent;
+}
+
+body::-webkit-scrollbar-thumb {
+  background-color: #888;
+  display: none;
+}
+
+body::-webkit-scrollbar-track {
+  background-color: #f1f1f1;
+  display: none;
+}
+</style>
 
 <style lang="scss" scoped>
 .tabs-icon-margin {
